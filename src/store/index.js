21b631d74166473a6b1a704b27bd2deacdc4534d
ricardo <rarrosr@gmail.com>
2021-04-16 17:48:10 -0400

    vue project init vue-router, vuex, axios, bootstrap, ...

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+
+const instance = axios.create({
+	baseURL: "https://randomuser.me",
+	timeout: 1000,
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    randomUser: []
   },
   mutations: {
+    DATA_RANDOM_USERS(state, data) {
+      state.randomUsers = data
+    }
   },
   actions: {
+    actionGetRandomUsers({ commit }) {
+      const getRandomUsers = instance.get('/api/?results=100&nat=us')
+      getRandomUsers.then((res) => {
+        commit('DATA_RANDOM_USERS', res.data.results)
+      })
+    }
   },
   modules: {
   }
