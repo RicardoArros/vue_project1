d3bf61544c21ba91035c6bef7c72debac9200255
ricardo <rarrosr@gmail.com>
2021-04-17 00:51:29 -0400

    Created UserList component, added filteredUsers in getters

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,10 +1,44 @@
 <template>
-  <div class="home"></div>
+  <div class="container">
+    <!-- FILTERS -->
+    <div class="row mx-0">
+      <div class="col-md-6 col-lg-4 mx-auto mt-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Buscar por nombre"
+          v-model="filters.searchQuery"
+        />
+      </div>
+    </div>
+
+    <!-- USERS CARDS -->
+    <div class="row mx-0">
+      <div
+        v-for="user in filteredUsers"
+        :key="user.id.value"
+        class="col-12 col-md-6 col-lg-4 p-3"
+      >
+        <div class="border rounded py-2">
+          <img :src="user.picture.large" alt="" class="mb-3" />
+          <h3>{{ `${user.name.first} ${user.name.last}` }}</h3>
+          <h6>{{ `${user.dob.age} años` }}</h6>
+          <h6>{{ `${user.location.state}` }}</h6>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapState, mapGetters } from "vuex";
+
 export default {
   name: "UserList",
+  computed: {
+    ...mapState(["randomUsers", "filters"]),
+    ...mapGetters(["filteredUsers"]),
+  },
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,23 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 
+//
 const instance = axios.create({
 	baseURL: "https://randomuser.me",
 	timeout: 1000,
-})
+});
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    randomUser: []
+    //
+    randomUsers: [],
+    //
+    filters: {
+      //
+      searchQuery: '',
+    }
   },
   mutations: {
     DATA_RANDOM_USERS(state, data) {
@@ -19,13 +26,53 @@ export default new Vuex.Store({
     }
   },
   actions: {
+    //
     actionGetRandomUsers({ commit }) {
-      const getRandomUsers = instance.get('/api/?results=100&nat=us')
+      //
+      const getRandomUsers = instance.get('/api/?results=100&nat=us');
+
+      //
       getRandomUsers.then((res) => {
         commit('DATA_RANDOM_USERS', res.data.results)
-      })
+
+        //
+        console.log(res.data.results);
+      });
+      
     }
   },
-  modules: {
+  getters: {
+    filteredUsers: (state) => {
+      //
+      let users = state.randomUsers;
+      let filteredUsers = [];
+      let activeFilters = false;
+
+      //
+      if(state.filters.searchQuery.length > 1) {
+        //
+        activeFilters = true;
+
+        //
+        filteredUsers = users.filter(user => {
+          //
+          let userName = `${user.name.first} ${user.name.last}`.toLowerCase();
+
+          //
+          let inputText = state.filters.searchQuery.toLowerCase();
+
+          //
+          return userName.includes(inputText);
+        });
+      }
+
+      //
+      if(activeFilters) {
+        return filteredUsers;
+
+      } else {
+        return users;
+      }
+    }
   }
-})
+});
